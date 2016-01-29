c81bd53cd9b8ec66cd677fb21507c102cb53de3e
Robert Lynch <robertlynch2012@gmail.com>
2016-01-28 21:20:59 -0600

    Fixed judge so it works, minus tests

--- judge.py
+++ judge.py
@@ -3,8 +3,16 @@ import os
 import queue
 import threading
 import subprocess
+import uuid
+import shlex
 from submission import Submission
 
+"""
+A judge is a test runner which takes a submission and enques it for
+verification of correctness. In order to avoid malicious code absuing the host,
+this runs in a docker container expected to be running. The actual judging
+happens in a background thread and is interacted with via a queue.
+"""
 class Judge:
 
     docker_name = 'sandbox'
@@ -37,7 +45,7 @@ class Judge:
     Run a given submission on docker
     """
     def run_judge(self, submission):
-        f = prepare_submission(submission)
+        f = self.prepare_submission(submission)
         file_path = os.path.dirname(f.name)
         file_name = os.path.basename(f.name)
 
@@ -60,6 +68,9 @@ class Judge:
             file_name
         ]
 
+        # Generate a test
+        test_in, test_out = Collatz.generate_tests()
+
         ran_to_completion = True
 
         # Create a timer to watch for timeouts
@@ -74,7 +85,8 @@ class Judge:
 
         # Run the docker test command in a new process
         runner = subprocess.Popen(
-            stdin=input_file,
+            shlex.split(' '.join(docker_cmd)),
+            stdin=test_in,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
@@ -83,12 +95,32 @@ class Judge:
         # We're done with everything, stop the timer
         timer.cancel()
 
+        # Verify the program's output if it completed
+        if ran_to_completion:
+            verify_output(submission, stdout, test_out)
+        else:
+            submission.status = Submission.TIMEOUT
+            submission.save()
+
     """
     Take a submission and turn the submission text into a file we can run
     against docker
     """
-    def prepare_submission(submission):
-        fp = tempfile.TemporaryFile()
-        fp.write(submission.submission_text)
+    def prepare_submission(self, submission):
+        fp = tempfile.NamedTemporaryFile()
+        fp.write(submission.submission_text.encode("UTF-8"))
         fp.seek(0)
         return fp
+
+    """
+    Verify the output of the program and update the submission with the status
+    """
+    def verify_output(self, submission, actual, expected):
+        if actual == expected:
+            submission.status = Submission.PASSED
+        else:
+            submission.status = Submission.FAILED
+            submission.actual_out = actual
+            submission.expected_out = expected
+
+        submission.save()
--- submission.py
+++ submission.py
@@ -3,8 +3,12 @@ from mongoengine import *
 class Submission(Document):
     WAITING = 'waiting'
     RUNNING = 'running'
-    FINISHED = 'finished'
+    TIMEOUT = 'timeout'
+    PASSED = 'passed'
+    FAILED = 'failed'
 
-    id = StringField(required=True)
+    id = StringField(required=True, primary_key=True)
     submission_text = StringField(required=True)
     status = StringField(default=WAITING, required=True)
+    actual_out = StringField()
+    expected_out = StringField()
