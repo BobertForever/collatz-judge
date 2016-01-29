cd1ef032cde9f6965b46fd91053df3ae5b0b8a35
Robert Lynch <robertlynch2012@gmail.com>
2016-01-28 21:21:13 -0600

    Have flask server accept the input and judge it

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from mongoengine import connect
 from judge import Judge
+from submission import Submission
+import uuid
 
 app = Flask(__name__)
 
@@ -9,8 +11,29 @@ connect('collatz')
 judge = Judge()
 
 @app.route("/")
-def hello():
+def index():
     return render_template('submit.html')
+
+@app.route("/submit",  methods=['POST'])
+def submit():
+    code = request.form['code']
+
+    # Generate a unique ID for the submission
+    id = str(uuid.uuid4())[:8]
+    while Submission.objects(id=id).count() > 0:
+        id = str(uuid.uuid4())[:8]
+
+    # save the submission and submit it for judging
+    submission = Submission(submission_text=code, id=id)
+    submission.save()
+    judge.submit(submission)
+
+    return redirect(url_for('view_submission', id=id))
+
+@app.route("/submission/<id>")
+def view_submission(id):
+    submission = Submission.objects(id=id).first()
+    return submission.submission_text
 
 if __name__ == "__main__":
     app.run(debug=True)
